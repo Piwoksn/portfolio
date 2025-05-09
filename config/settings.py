d98ffa413c392356b1117ea1cc3b613103126632
Piwoksn <piwoksn@gmail.com>
2025-05-09 22:31:13 +0100

    updated

--- config/settings.py
+++ config/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-$gzw1n-9_f#==o*4fn&segise#41b=b55ubjvn2#kkv_hrlgjy'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['piwoksnoble.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
